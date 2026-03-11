e6980ad06ab82b99e9c2525c14e3e2d78c912c0c
Hasnainskhan <Hasnainbabar746@gmail.com>
2026-03-12 04:15:58 +0500

    website icon changed

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+document.title = 'Engineers Hub'
+const link = document.querySelector<HTMLLinkElement>('link[rel="icon"]') ?? document.createElement('link')
+link.rel = 'icon'
+link.type = 'image/png'
+link.href = '/logo.png?v=2'
+if (!document.querySelector('link[rel="icon"]')) document.head.appendChild(link)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
